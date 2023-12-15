8fb8af8bc3cb047b98b1b2054e613f6b1ba461c2
Dev Expert <128534413+DevExpert0101@users.noreply.github.com>
2023-12-15 15:17:55 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,8 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Setting the API key
-openai.api_key = os.getenv('OPENAI_API_KEY')
+
 
 app.add_middleware(
     CORSMiddleware,
