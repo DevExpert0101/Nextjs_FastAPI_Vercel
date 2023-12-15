33400f3d6dc8f16dcde1b329efa09e1e26659506
Dev Expert <128534413+DevExpert0101@users.noreply.github.com>
2023-12-15 15:54:06 -0500

    changed openai to request

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
-import openai
+
 from fastapi import FastAPI
 import json
 
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai_key = os.getenv('OPENAI_API_KEY')
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,12 +26,44 @@ class SummarizeSchema(BaseModel):
     prompt: str
     inst: str
 
+def Gpt_API(prompt, instruction):
+    openai_endpoint = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"
+    headers = {
+        "Authorization": f"Bearer {openai_key}",
+        "Content-Type": "application/json",
+    }
+
+    data = {
+        "prompt": f"{prompt} \n\n {instructions}",
+        "temperature": 0.7,
+    }
+
+    response = requests.post(openai_endpoint, headers=headers, data=json.dumps(data))
+    answer = respons['choices'][0]['text']
+    return answer
 
 
 @app.post("/api/shorten")
 async def shorten(info: Prompt):
     prompt = info.prompt
-
+    instruction = ''''Shorten if the above sentence in the paragraph is larger.    If so, suggest a shorter sentence. Please output only shorten senences and json format like this:'
+   
+        {
+            "sentence":    "The research on    our    universe is    still    ongoing
+                on    scientists    are constantly    discovering new    facts    and learning
+                more about    our    galaxy    and beyond."
+        }
+        or
+        {
+            "sentences":    [
+                "The research on    our    universe is    ongoing and scientists
+                are constantly    learning more about    it.",
+                "Scientists    are constantly    learning more about    our
+                universe."
+            ]
+        }
+        '''
+    rlt = Gpt_API(prompt, instruction)
     
-    return prompt
+    return rlt
 
